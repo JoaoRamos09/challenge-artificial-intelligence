68113ae23d66576c0a3f473e2e8c8dbb5c062530
Joao <joaorsramos@Hotmail.com>
2025-07-26 18:11:51 -0300

    Refactors the text processing service to integrate AIService and adds the ExtractTagsDTO class for structuring extracted tag data.

--- app/controller/data_processing_controller.py
+++ app/controller/data_processing_controller.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Depends
 from app.service.text_processing_service import TextProcessingService
+from app.service.ai_service import AIService
 
 processing_data_router = APIRouter(prefix="/data-processing", tags=["process-data"])
 
-def get_indexing_service():
-    return TextProcessingService()
+def get_ai_service():
+    return AIService()
+
+def get_indexing_service(ai_service: AIService = Depends(get_ai_service)):
+    return TextProcessingService(ai_service)
 
 @processing_data_router.post("/text")
 def process_text(path: str, processing_text_service: TextProcessingService = Depends(get_indexing_service)):
--- app/service/text_processing_service.py
+++ app/service/text_processing_service.py
@@ -1,18 +1,19 @@
 from pathlib import Path
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from app.dto.text_dto import TextDTO
+from app.dto.chunk_dto import ChunkDTO
+from app.service.ai_service import AIService
 
 class TextProcessingService:
-    def __init__(self):
+    def __init__(self, ai_service: AIService):
+        self.ai_service = ai_service
         pass
 
     def process_text(self, file_name: str):
         try:
             text = self.get_text_from_path(file_name)
-            chunks = self.split_text(text)
-            texts_dto = [TextDTO(content=chunk, path=file_name, tags=[], metadata={}) for chunk in chunks]
-            ##TODO: Extract tags and metadata from service AI
-            return texts_dto
+            split_texts = self.split_text(text)
+            chunks = self.texts_to_chunks(texts=split_texts,file_name=file_name)
+            return chunks
         except FileNotFoundError as e:
             raise e
         except Exception as e:
@@ -22,12 +23,20 @@ class TextProcessingService:
         file_path = Path(file_name)
         with open(file_path, "r") as file:
             return file.read()
-        
+    
     
     def split_text(self, text: str):
         text.split()
         splitter = RecursiveCharacterTextSplitter(chunk_size=450, chunk_overlap=50)
         return splitter.split_text(text)
+    
+    def texts_to_chunks(self, texts:list, file_name:str):
+        chunks = []
+        for text in texts:
+            response = self.ai_service.extract_tags_by_llm(text)
+            text_dto = ChunkDTO(content=text, path= file_name, technical_level=response.technical_level, tags=response.subject, metadata={})
+            chunks.append(text_dto)
+        return chunks
     
     
     
