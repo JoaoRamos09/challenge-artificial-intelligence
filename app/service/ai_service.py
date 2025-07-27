645c4e430443fabeba8ecbc67d3c1425292b9d29
Joao <joaorsramos@Hotmail.com>
2025-07-26 22:12:58 -0300

    Refactors AIService to support multiple LLM providers, including OpenAI and Groq, and adjusts the invoke_llm method signature to accept provider and model parameters.

--- app/service/ai_service.py
+++ app/service/ai_service.py
@@ -1,48 +1,34 @@
 from langchain_groq import ChatGroq
-from langchain_core.messages import HumanMessage, SystemMessage
-from app.dto.exctract_tags_dto import ExtractTagsDTO
+from langchain_openai import ChatOpenAI
 
 default_model = "meta-llama/llama-4-maverick-17b-128e-instruct"
+default_provider = "groq"
 
 class AIService():
     def __init__(self) -> None:
         pass
     
-    def invoke_llm(self,messages, output_structured=None):
+    def invoke_llm(self,messages,provider:str = default_provider, model:str = default_model,output_structured=None):
             try:
-                result = self.get_llm(output_structured).invoke(input=messages)
+                result = self.get_llm(provider,output_structured, model).invoke(input=messages)
                 return result
             ##TODO: Add custom exception
             except Exception as e:
                 raise e
     
-    def get_llm(self,output_structured:str, model:str = default_model):
+    def get_llm(self,provider:str, output_structured, model:str):
         try:
-            return ChatGroq(model=model).with_structured_output(output_structured)
-        ##TODO: Add custom exception
-        except Exception as e:
-            raise
-    
-    def extract_tags_by_llm(self,text:str):
-        messages = [
-            SystemMessage(
-                content=
-                """
-                Você é um extrator de palavras-chaves de textos voltados para a área de programaçaõ e tecnologia, você irá analisar um texto e coletar as seguintes informações dele:
-                
-                **Tags de Nível Técnico: Qual é o nível técnico necessário para entender o texto? ['easy','intermediare','hard']
-                **Tags de Tópicos: Quais são os principais tópicos do text? Exemplo: 'html', 'introduction' 'python' 
-                         """
-            ),
-            HumanMessage(
-                content=
-                f"""
-                Análise o texto e extrai as tags:
-                {text}
-                """
-            )
-        ]
-        return self.invoke_llm(messages=messages, output_structured=ExtractTagsDTO)
+            if provider == "openai":
+                if output_structured:
+                    return ChatOpenAI(model=model).with_structured_output(output_structured)
+                return ChatOpenAI(model=model)
             
+            else:
+                if output_structured:
+                    return ChatGroq(model=model).with_structured_output(output_structured)
+                return ChatGroq(model=model)
+        ##TODO: Add custom exception    
+        except Exception as e:
+            raise e
     
     
