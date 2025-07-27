06e8011c7145895073b9ad8152461c88c9eaec24
Joao <joaorsramos@Hotmail.com>
2025-07-27 13:14:18 -0300

    Adds global exception handlers for application error handling, including custom and validation exception handling, improving error handling for the client.

---
+++ app/handler/global_handler.py
@@ -0,0 +1,29 @@
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from app.exception.base_exception import BaseException
+from pydantic import ValidationError
+
+async def global_exception_handler(request: Request, exc: BaseException):
+    return JSONResponse(
+        status_code=exc.status_code, 
+        content={
+            "message": exc.detail, 
+            "details": exc.details})
+
+async def handle_validation_exception(request: Request, exc: ValidationError):
+    return JSONResponse(
+        status_code=422,
+        content={
+            "message": "Validation error",
+            "details": {"validation_errors": exc.errors()}
+        }
+    )
+
+async def handle_generic_exception(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={
+            "message": "Internal server error",
+            "details": {}
+        }
+    )
