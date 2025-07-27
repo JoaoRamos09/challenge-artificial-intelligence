501b485a9129f63965d2ab04dc435045ca82b328
Joao <joaorsramos@Hotmail.com>
2025-07-27 15:19:52 -0300

    Adds the PineconeException class and subclasses for handling Pinecone-specific errors, improving code robustness when handling index not found exceptions and upsert errors.

---
+++ app/exception/pinecone_exception.py
@@ -0,0 +1,40 @@
+from app.exception.base_exception import BaseException
+from typing import Optional
+from fastapi import status
+
+class PineconeException(BaseException):
+    
+    def __init__(self, 
+                 message: str, 
+                 namespace: Optional[str] = None,
+                 index: Optional[str] = None,
+                 error_code: str = "PINECONE_ERROR",
+                 status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR,
+                 service: Optional[str] = "PINECONE"
+        ):
+        details = {"namespace": namespace, "index": index} if namespace and index else {}
+        super().__init__(
+            message, 
+            status_code=status_code, 
+            details=details,
+            error_code=error_code,
+            service=service
+        )
+        
+        
+class PineconeIndexNotFound(PineconeException):
+    def __init__(self, index: str):
+        super().__init__(
+            message=f"Index not found",
+            index=index,
+            error_code="PINECONE_INDEX_NOT_FOUND",
+        )
+
+class PineconeUpsertError(PineconeException):
+    def __init__(self, namespace: str, index: str):
+        super().__init__(
+            message=f"Error upserting data into Pinecone",
+            namespace=namespace,
+            index=index,
+            error_code="PINECONE_UPSERT_ERROR",
+        )
