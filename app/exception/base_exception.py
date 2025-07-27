20a33b0819a0c3e8ec003817fc2ab2d1288b3a4a
Joao <joaorsramos@Hotmail.com>
2025-07-27 13:13:38 -0300

    Adds the BaseException class for managing custom exceptions in the application, allowing the inclusion of error codes, details and information about the service.

---
+++ app/exception/base_exception.py
@@ -0,0 +1,11 @@
+from fastapi import HTTPException, status
+from typing import Optional, Dict, Any
+
+class BaseException(HTTPException):
+    def __init__(self, message: str, status_code:int = status.HTTP_500_INTERNAL_SERVER_ERROR, error_code:Optional[str] = None, details: Optional[Dict[str, Any]] = None, service: Optional[str] = None):
+        self.error_code = error_code or f"HTTP_{status_code}"
+        self.details = details or {}
+        self.service = service
+        super().__init__(status_code=status_code, detail=message)
+        
+        
