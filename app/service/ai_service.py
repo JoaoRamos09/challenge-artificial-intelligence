0c398a8f916a5dffe370ca840d8e1acc0d1aa32b
Joao <joaorsramos@Hotmail.com>
2025-07-27 11:52:13 -0300

    Adds the invoke_whisper method to the AIService class for transcribing audio using the OpenAI library.

--- app/service/ai_service.py
+++ app/service/ai_service.py
@@ -1,5 +1,6 @@
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
+from openai import OpenAI
 
 default_model = "meta-llama/llama-4-maverick-17b-128e-instruct"
 default_provider = "groq"
@@ -30,5 +31,15 @@ class AIService():
         ##TODO: Add custom exception    
         except Exception as e:
             raise e
+    
+    def invoke_whisper(self,path_file):
+        client = OpenAI()
+        transcription = client.audio.transcriptions.create(
+        model="gpt-4o-mini-transcribe", 
+        file=path_file, 
+        response_format="text"
+        )
+        
+        return transcription
     
     
