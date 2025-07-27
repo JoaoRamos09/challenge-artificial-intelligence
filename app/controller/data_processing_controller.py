8ca99aabadb3d0ee8999a5dea416ec858c256996
Joao <joaorsramos@Hotmail.com>
2025-07-27 11:52:57 -0300

    Adds the video processing service and integrates it with the data processing controller, enabling video analysis with structured feedback. Implements methods for video transcription and analysis, using the AIService and TextProcessingService.

--- app/controller/data_processing_controller.py
+++ app/controller/data_processing_controller.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from app.service.text_processing_service import TextProcessingService
 from app.service.image_processing_service import ImageProcessingService
+from app.service.video_processing_service import VideoProcessingService
 from app.service.ai_service import AIService
 
 processing_data_router = APIRouter(prefix="/data-processing", tags=["process-data"])
@@ -14,6 +15,9 @@ def get_text_processing_service(ai_service: AIService = Depends(get_ai_service))
 def get_image_processing_service(ai_service: AIService = Depends(get_ai_service)):
     return ImageProcessingService(ai_service)
 
+def get_video_processing_service(ai_service: AIService = Depends(get_ai_service), text_processing_service: TextProcessingService = Depends(get_text_processing_service)):
+    return VideoProcessingService(ai_service, text_processing_service)
+
 @processing_data_router.post("/text")
 def process_text(path: str, text_processing_service: TextProcessingService = Depends(get_text_processing_service)):
     text = text_processing_service.process_text(file_name=path)
@@ -22,4 +26,9 @@ def process_text(path: str, text_processing_service: TextProcessingService = Dep
 @processing_data_router.post("/image")
 def process_image(path:str, image_processing_service: ImageProcessingService = Depends(get_image_processing_service)):
     response = image_processing_service.process_image(path)
+    return response
+
+@processing_data_router.post("/video")
+def process_video(path:str, video_processing_service: VideoProcessingService = Depends(get_video_processing_service)):
+    response = video_processing_service.process_video(path)
     return response
---
+++ app/service/video_processing_service.py
@@ -0,0 +1,77 @@
+from app.service.ai_service import AIService
+from pathlib import Path
+from langchain_core.messages import HumanMessage, SystemMessage
+from app.dto.video_analysis_dto import VideoAnalysisDTO
+from app.dto.chunk_dto import ChunkDTO
+from app.service.text_processing_service import TextProcessingService
+from typing import List
+class VideoProcessingService():
+    def __init__(self, ai_service: AIService, text_processing_service: TextProcessingService):
+        self.ai_service = ai_service
+        self.text_processing_service = text_processing_service
+    
+    def process_video(self,path:str):
+        video_file = self.get_video_from_path(path)
+        transcription = self.ai_service.invoke_whisper(video_file)
+        chunks = self.transcription_to_chunks(transcription, path)
+
+        return chunks
+    
+    ##Whisper close the file after use
+    def get_video_from_path(self, path:str):
+        file_path = self.exists_file(path)
+        video_file = open(file_path, "rb")
+        return video_file
+    
+    def transcription_to_chunks(self,transcription:str, path:str):
+        split_transcription = self.text_processing_service.split_text(transcription)
+        chunks = []
+        for chunk in split_transcription:
+            analysis = self.get_analysis_video(chunk)
+            chunk_dto = ChunkDTO(content=chunk, path=path, technical_level="", tags=analysis.tags, metadata=self.format_metadata_dict(analysis.summary, analysis.subject))
+            chunks.append(chunk_dto)
+        return chunks
+    
+    def format_metadata_dict(self, summary:str, subject:List[str]):
+        return {
+            "summary": summary if summary is not None else "",
+            "subject": subject if subject is not None else [],
+        }
+    ##TODO: Add service file
+    def exists_file(self,path:str):
+        file_path = Path(path)
+        
+        if not file_path.exists():
+            raise FileNotFoundError
+        
+        if not file_path.is_file():
+            raise ValueError
+        
+        return file_path
+    
+    def get_analysis_video(self,transcription:str):
+        messages = [
+            SystemMessage(content="""
+                          Você é um especialista em análise de transcrições de vídeos.
+                          Sua função é analisar a transcrição e fornecer um resumo, assunto e nível técnico.
+                          
+                          FORMATO EXATO:
+                          {
+                              "summary": "Resumo da transcrição",
+                              "subject": "Assunto da transcrição",
+                              "technical_level": "Nível técnico da transcrição"
+                          }
+                          
+                          REGRAS OBRIGATÓRIAS:
+                          1. Responda APENAS o JSON, sem texto adicional
+                          2. Inclua SEMPRE os três campos: "summary", "subject" e "technical_level"
+                          3. "subject" deve ser uma lista de strings com os assuntos principais do vídeo (ex: ["tecnologia", "programacao"])
+                          4. "tags" deve ser uma lista de strings com as palavras chaves do vídeo (ex: ["tecnologia", "programacao"])
+                          5. "summary" deve ser um resumo do vídeo com no máximo
+                          5. Certifique-se de que o JSON está completo e válido
+                          6. Não corte a resposta no meio
+                          """),
+            HumanMessage(content=transcription)
+        ]
+        
+        return self.ai_service.invoke_llm(messages=messages, output_structured=VideoAnalysisDTO, provider="openai", model="gpt-4o-mini")
